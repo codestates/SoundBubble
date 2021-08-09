4be16dd7f1c5ba878d10335c5ec025b8d3986df1
jeongbeen2 <aceman9508@gmail.com>
2021-08-09 15:40:11 +0900

    [Feat] TypeScript-Redux initial setting

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootReducerType } from './Store'
+import  {increaseNumber,decreaseNumber} from './actions/index'
+
+function App() {
+
+const numberReducer = useSelector((state:RootReducerType) => state.numberReducer)
+  const dispatch = useDispatch();
+  
+
+  const changeNum = (event: any) => {
+    if(event.target.innerText === '-') dispatch(decreaseNumber())
+    if(event.target.innerText === '+') dispatch(increaseNumber())
+  }
+  
+  return (
+    <>
+      <div className='main'>
+        <h1>{numberReducer.num}</h1>
+      <button onClick={changeNum}>-</button>
+      <button onClick={changeNum}>+</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
