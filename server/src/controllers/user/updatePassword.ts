170dda8393b0332d992be8c8c90bab8ceef30b76
OhDaky <ttakko88@gmail.com>
2021-08-10 13:53:46 +0900

    [Feat] 닉네임 수정 컨트롤러 작성

--- server/src/controllers/user/updatePassword.ts
+++ server/src/controllers/user/updatePassword.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import crypto = require("crypto");
+import { User } from "../../entity/User";
 
 const updatePassword = async (req: Request, res: Response) => {
 
