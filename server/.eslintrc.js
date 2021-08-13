12b0cf61d35614212ebb89b3ba56748708c6de77
idenk <idenkim@gmail.com>
2021-08-13 17:52:04 +0900

    [Fixed] ts-lint 적용

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -14,8 +14,8 @@ module.exports = {
 		"no-empty": "error", // 빈 블록문을 지양한다.
 		"default-case": "error", // switch 문에 default가 포함되어야 한다.
 		"default-case-last": "error", // switch 문에 default가 항상 마지막에 작성되어야 한다.
-        eqeqeq: ["error", "always"], // '==' 밎 '!=' 사용을 지양한다.
-        "@typescript-eslint/no-unused-vars": "off",
+		eqeqeq: ["error", "always"], // '==' 밎 '!=' 사용을 지양한다.
+		"@typescript-eslint/no-unused-vars": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/explicit-function-return-type": "off",
 	},
