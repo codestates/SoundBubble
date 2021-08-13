ef6ee353c024e8523be2e34e28a0ad52b34700e3
jeongbeen2 <aceman9508@gmail.com>
2021-08-13 17:00:19 +0900

    [Bug] prettier 적용오류 해결

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
 	parser: "@typescript-eslint/parser",
-	extends: ["plugin:@typescript-eslint/recommended"],
+	extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
 	},
 	rules: {
 		"no-empty": "error", // 빈 블록문을 지양한다.
@@ -11,7 +14,12 @@ module.exports = {
 		"default-case-last": "error", // switch 문에 default가 항상 마지막에 작성되어야 한다.
 		eqeqeq: ["error", "always"], // '==' 밎 '!=' 사용을 지양한다.
 		"prefer-template": "error", // 문자열 연결 연산자를 지양하고 템플릿 리터럴을 사용한다.
+		"react/prop-types": "off",
 		"no-var-requires": true,
 	},
-	settings: {},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
