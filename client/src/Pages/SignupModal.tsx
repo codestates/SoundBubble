472b3bf53b313e196be6c343259ceac916caef37
jeongbeen2 <aceman9508@gmail.com>
2021-08-16 00:09:26 +0900

    [Feat] Modal 구현

--- client/src/Pages/SignupModal.tsx
+++ client/src/Pages/SignupModal.tsx
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { emailIsValid, pwIsValid } from "../Utils/Validator";
 import "./Styles/SignupModal.css";
+import Modal from "../Components/Modal";
 
 const SignupModal = (): JSX.Element => {
 	const [name, setName] = useState("");
@@ -13,6 +14,11 @@ const SignupModal = (): JSX.Element => {
 	const [errorMsg, setErrorMsg] = useState("");
 	const URL = process.env.REACT_APP_API_URL;
 	const history = useHistory();
+	const [isModal, setIsModal] = useState(false);
+
+	const handleCloseModal = () => {
+		setIsModal(false);
+	};
 
 	const handleSamePW = (PW: string, RePW: string) => {
 		return PW === RePW ? true : false;
@@ -22,6 +28,7 @@ const SignupModal = (): JSX.Element => {
 		setErrorMsg("");
 		if (!emailIsValid(ID)) {
 			setErrorMsg("ID는 이메일 형식입니다.");
+			setIsModal(true);
 			return;
 		}
 		if (!pwIsValid(PW)) {
@@ -47,41 +54,44 @@ const SignupModal = (): JSX.Element => {
 	};
 
 	return (
-		<div className="signup-body">
-			<aside className="signup-sidebar-content">
-				<header className="signup-header"></header>
-				<img />
-			</aside>
-			<main className="signup-main">
-				<div className="signup-content">
-					<h2>Sign up to soundBubble</h2>
-					<hr className="divider" />
-					<fieldset className="signup-user-name">
-						<label className="signup-label">Name</label>
-						<input className="signup-input-name" type="text" onChange={e => setName(e.target.value)} />
-					</fieldset>
-					<fieldset className="signup-user-email">
-						<label className="signup-label">Email</label>
-						<input className="signup-input-email" type="email" onChange={e => setID(e.target.value)} />
-					</fieldset>
-					<div className="signup-password-group">
-						<fieldset className="signup-user-password">
-							<label className="signup-label">Password</label>
-							<input className="signup-input-password" type="password" onChange={e => setPW(e.target.value)} />
+		<>
+			{isModal ? <Modal handleCloseModal={handleCloseModal} /> : null}
+			<div className="signup-body">
+				<aside className="signup-sidebar-content">
+					<header className="signup-header"></header>
+					<img />
+				</aside>
+				<main className="signup-main">
+					<div className="signup-content">
+						<h2>Sign up to soundBubble</h2>
+						<hr className="divider" />
+						<fieldset className="signup-user-name">
+							<label className="signup-label">Name</label>
+							<input className="signup-input-name" type="text" onChange={e => setName(e.target.value)} />
 						</fieldset>
-						<fieldset className="signup-user-RePassword">
-							<label className="signup-label">Re-Password</label>
-							<input className="signup-input-RePassword" type="password" onChange={e => setRePW(e.target.value)} />
+						<fieldset className="signup-user-email">
+							<label className="signup-label">Email</label>
+							<input className="signup-input-email" type="email" onChange={e => setID(e.target.value)} />
 						</fieldset>
+						<div className="signup-password-group">
+							<fieldset className="signup-user-password">
+								<label className="signup-label">Password</label>
+								<input className="signup-input-password" type="password" onChange={e => setPW(e.target.value)} />
+							</fieldset>
+							<fieldset className="signup-user-RePassword">
+								<label className="signup-label">Re-Password</label>
+								<input className="signup-input-RePassword" type="password" onChange={e => setRePW(e.target.value)} />
+							</fieldset>
+						</div>
+						<div className="signup-alert-box">{errorMsg}</div>
+						<div className="signup-form-btn">
+							<button onClick={handleSignUp}>Create Account</button>
+							<button onClick={() => history.push("/login")}>Go To Login</button>
+						</div>
 					</div>
-					<div className="signup-alert-box">{errorMsg}</div>
-					<div className="signup-form-btn">
-						<button onClick={handleSignUp}>Create Account</button>
-						<button onClick={() => history.push("/login")}>Go To Login</button>
-					</div>
-				</div>
-			</main>
-		</div>
+				</main>
+			</div>
+		</>
 	);
 };
 
