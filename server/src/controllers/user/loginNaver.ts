3026cd66c88bba7fd73a048a45c71c7c355af89f
OhDaky <ttakko88@gmail.com>
2021-08-16 16:06:37 +0900

    [Feat] 네이버 로그인 작성

---
+++ server/src/controllers/user/loginNaver.ts
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { Request, Response, RequestHandler } from "express";
+import { User } from "../../entity/User";
+import { UserToken } from "../../entity/UserToken";
+
+const loginNaver: RequestHandler = async (req: Request, res: Response) => {
+  //* 클라이언트로부터 Authorization Code 획득
+  const { authorizationCode }: { authorizationCode: string } = req.body;
+
+  try {
+    //* 파라미터 검사
+    if (!authorizationCode) {
+      return res.status(400).json({ message: "Code(body) does not exist" });
+    }
+
+    //* 네이버 아이디 로그인 버튼 생성
+    const NaverClientId = process.env.NAVER_CLIENT_ID as string;
+    const NaverClientSecret = process.env.NAVER_CLIENT_SECRET as string;
+    const NaverRedirectUri = process.env.NAVER_REDIRECT_URI as string;
+    const NaverState = 'naverstate';
+
+    //* 토큰 발급
+    const { data: { accessToken, refreshToken }, } = await axios({
+      url: "http://nid.naver.com/oauth2.0/token",
+      method: "post",
+      params: {
+        client_id: NaverClientId,
+        client_secret: NaverClientSecret,
+        code: authorizationCode,
+        redirect_uri: NaverRedirectUri,
+      },
+    });
+
+    //* 유저 정보 요청
+    const profile = await axios({
+      url: "https://openapi.naver.com/v1/nid/me",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      }
+    })
+
+    //* 회원가입된 유저인지 확인
+    const email = profile.data.response.email;
+    const user: User | undefined = await User.findOne({ email });
+
+    if (!user) {
+      const nickname = profile.data.response.nickname;
+      const profileImage = profile.data.response.profileImage;
+      
+      if (profileImage) {
+        await User.insertUser(email, "", nickname, "naver", "user", profileImage);
+      } else {
+        await User.insertUser(email, "", nickname, "naver", "user");
+      }
+      res.status(201);
+    } else {
+      res.status(200);
+    }
+
+    //* 유저 존재. 소셜 로그인 대신 먼저 이메일로 가입한 유저
+    //? 이메일 도용 문제 존재
+    //! 일반 회원가입 -> 소셜 로그인 가능. 소셜 로그인 -> 일반 회원가입 불가.(소셜 로그인 유저는 비밀번호 변경을 통해 일반 로그인 가능)
+    if (user && user.signUpType === "email") {
+      user.signUpType = "intergration"; // 계정 통합
+      await user.save();
+    }
+
+    //* refreshToken 저장
+    const userInfo = (await User.findUserByEmail(email)) as User;
+    await UserToken.insertToken(userInfo.id, refreshToken);
+
+    return res.json({ data: { accessToken, userInfo }, message: "Login succeed" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Faild to Naver login" });
+  }
+};
+
+export default loginNaver;
