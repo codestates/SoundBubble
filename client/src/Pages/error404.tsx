c98209ec712ed673ca7abd34b1d4f159a543c261
OhDaky <ttakko88@gmail.com>
2021-08-20 01:40:32 +0900

    [Style] error 페이지 css 수정

--- client/src/Pages/error404.tsx
+++ client/src/Pages/error404.tsx
@@ -11,15 +11,27 @@ const error404 = (): JSX.Element => {
 	}
 	
 	return (
-		<div className="errorWrapper">
-			<div className="errorMessage">
-				<div className="error404">404</div>
-				<div className="page">잘못된 주소입니다!</div>
-				<div className="page">아래 페이지로 이동하세요</div>
+		<div>
+			<div className="error-bubbles">
+				<div className="error-bubble b2" />
+				<div className="error-bubble b5" />
+				<div className="error-bubble" />
+				<div className="error-bubble b6" />
+				<div className="error-bubble b3" />
+				<div className="error-bubble b7" />
+				<div className="error-bubble b4" />
+				<div className="error-bubble b8" />
 			</div>
-			<div className="error-btnWrap">
-				<button className="btn-go palette" onClick={goPalette}>팔레트 구경가기</button>
-				<button className="btn-go main" onClick={goMain}>체험하러 가기</button>
+			<div className="errorWrapper">
+				<div className="errorMessage">
+					<div className="error404">404</div>
+					<div className="page">잘못된 주소입니다!</div>
+					<div className="page">아래 페이지로 이동하세요</div>
+				</div>
+				<div className="error-btnWrap">
+					<button className="btn-go palette" onClick={goPalette}>팔레트 구경가기</button>
+					<button className="btn-go main" onClick={goMain}>체험하러 가기</button>
+				</div>
 			</div>
 		</div>
 	);
