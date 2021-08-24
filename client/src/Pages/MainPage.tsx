a167fe69c04e2ad2b57c694ef0cb4287c4f2090f
idenk <idenkim@gmail.com>
2021-08-24 10:51:50 +0900

    [Feat] 가이드 모달 창 레이아웃 생성

--- client/src/Pages/MainPage.tsx
+++ client/src/Pages/MainPage.tsx
@@ -8,6 +8,7 @@ import Footer from "../Components/Footer";
 
 import "./Styles/MainPage.css";
 import MainSelectBar from "../Components/MainPage/MainSelectBar";
+import MainTutorial from "../Components/Tutorial/MainTutorial";
 
 interface Select {
 	select: string;
@@ -24,12 +25,27 @@ function ChoiceMode(props: Select): JSX.Element {
 
 const MainPage = (): JSX.Element => {
 	const [select, setSelect] = useState("piano");
+	const [openTutorial, setOpenTutorial] = useState<boolean>(true);
+
+	//* 임시 코드: 모달창 다시 열기
+	// localStorage.setItem("showTutorial", "true");
+	//* --------------------------------------
+
 	const handleBtnClick = (e: React.ChangeEvent<HTMLButtonElement>) => {
 		setSelect(e.target.classList.value);
 	};
 
+	const handleCloseTutorial = (): void => {
+		setOpenTutorial(false);
+	};
+
 	return (
 		<>
+			{openTutorial && localStorage.getItem("showTutorial") !== "false" ? (
+				<MainTutorial handleCloseTutorial={handleCloseTutorial} checkboxOption={true} />
+			) : (
+				""
+			)}
 			<Navigation />
 			<div className="main-page">
 				<MainSelectBar select={select} handleBtnClick={handleBtnClick} />
