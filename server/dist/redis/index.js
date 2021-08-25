671806afbd062f15f103872561f1fefccaf44bf6
idenk <idenkim@gmail.com>
2021-08-25 16:48:58 +0900

    [Fixed] 토큰 화이트리스트 수정

--- server/dist/redis/index.js
+++ server/dist/redis/index.js
@@ -88,7 +88,10 @@ const checkWhiteList = async (userId, accessToken) => {
         const idx = list.white.indexOf(accessToken);
         if (idx >= 0) {
             log_1.log(`[유저 ${userId}] 토큰 화이트리스트: 토큰 존재`);
+            console.log("idx", idx);
+            console.log("삭제 전", list.white);
             list.white.splice(idx, 1); // 조회한 토큰 삭제
+            console.log("삭제 후", list.white);
             await exports.setAsync(String(userId), JSON.stringify(list));
             return true;
         }
